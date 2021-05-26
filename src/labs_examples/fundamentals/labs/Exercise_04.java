7589a08278025b1155a95c90ed24ca7e5e2feaca
Mehmet Keles <mehmetk6158@gmail.com>
2021-05-26 13:04:17 -0400

    My Second Java Coding

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,11 +1,12 @@
 package labs_examples.fundamentals.labs;
 
+import java.sql.SQLOutput;
+
 /**
  * Fundamentals Exercise 4: My Second Program
- *
- *      Write the necessary code to display, declare and print each of Java's primitive data types.
- *      Please declare a single variable of each type, give it an appropriate value, then print that variable.
- *
+ * <p>
+ * Write the necessary code to display, declare and print each of Java's primitive data types.
+ * Please declare a single variable of each type, give it an appropriate value, then print that variable.
  */
 
 public class Exercise_04 {
@@ -16,6 +17,38 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+
+        int x = 15;
+
+        System.out.println("int x is: " + x);
+
+        boolean besiktasWins = true;
+
+        System.out.println("besiktas always wins: " + besiktasWins);
+
+        char letter ='A' ;
+
+        System.out.println("letter = " +letter);
+
+        byte X = 123;
+
+        System.out.println("x equals " + X);
+
+        short Y = 12345;
+
+        System.out.println("Y :" + Y);
+
+        long Z = 12345678;
+
+        System.out.println("Z : " + Z);
+
+        float M = 123.567f;
+
+        System.out.println("M :" +M);
+
+        double N = 11111111.2233;
+
+        System.out.println("N is " + N);
 
     }
 
